b7595ef939ae97001cbb5df8ca80bd47798fa10f
stubborn123 <31369372+stubborn123@users.noreply.github.com>
2025-10-27 07:39:08 +0800

    sepolia部署合约

--- contract/hardhatone/README.md
+++ contract/hardhatone/README.md
@@ -11,3 +11,40 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat ignition deploy ./ignition/modules/Lock.ts
 ```
+
+（1）本地部署
+启动本地节点
+```
+npx hardhat  node
+```
+
+本地部署
+```
+npx hardhat ignition deploy ./ignition/modules/xxx.ts --network localhost
+```
+
+本地验证合约没有区块链浏览器，没有意义，部署到区块链网络上再验证。
+
+
+（2）在链上部署
+在sepolia上部署
+
+获取测试token
+
+配置rpc地址，如果不是公共网就用别的平台（一般会有免费额度）
+
+部署合约在链上，以sepolia为例
+```
+npx hardhat ignition deploy ignition/modules/counter.ts --network sepolia
+```
+
+验证合约
+```
+npx hardhat verify --network sepolia <YOUR_CONTRACT_ADDRESS>
+```
+也可以在对应的区块链浏览器
+
+
+(3) 注意事项
+注意不要硬编码把私钥暴露出来，可以把这个.env放到忽略
+
--- contract/hardhatone/hardhat.config.ts
+++ contract/hardhatone/hardhat.config.ts
@@ -1,8 +1,40 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
+// 通过dotenv来读取env的配置
+// 标准TypeScript导入方式
+import * as dotenv from "dotenv";
+// 加载.env文件中的环境变量
+dotenv.config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+
+  networks: {
+    hardhat :{},
+
+    // Sepolia测试网配置
+    sepolia: {
+      url: process.env.SEPOLIA_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+       timeout: 60000, // 增加超时时间到60秒
+    },
+
+    // 测试网配置
+    rootHashTest: {
+      url: process.env.RootHashTest_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+
+    // 替换原来的etherscan配置
+    etherscan: {
+      apiKey: {
+        sepolia: process.env.ETHERSCAN_API_KEY
+      },
+      timeout: 120000 // 增加超时时间到120秒
+    },
+
 };
 
 export default config;
